f38ebfca87a1fbcf01f134004f7a46a060e436f0
Olga Manaenkova <manaenkova.olga1@gmail.com>
2023-03-08 16:36:30 -0300

    some styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  flat = Flat.create!(name: Faker::Artist.name, address: Faker::Address.full_address, description: Faker::Hacker.say_something_smart, price_per_night: rand(100..80000), number_of_guests: rand(1..8))
+  flat.save
+end
